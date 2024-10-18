47303abfd34897ae4fca0e1e6d3746be792dba96
Andrey Odintsov <morrigun0@gmail.com>
2024-10-19 00:07:51 +0300

    Add project files.

---
+++ Morze/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Morze;
+
+Translator translator = new Translator();
+
+Console.WriteLine(translator.ToMorze("hello"));
+Console.WriteLine(translator.ToNormal(".... . .-.. .-.. ---"));
---
+++ Morze/Translator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Morze
+{
+    internal class Translator
+    {
+        static Dictionary<char, string> morseCodeDict = new Dictionary<char, string>
+        {
+            {'A', ".-"},
+            {'B', "-..."},
+            {'C', "-.-."},
+            {'D', "-.."},
+            {'E', "."},
+            {'F', "..-."},
+            {'G', "--."},
+            {'H', "...."},
+            {'I', ".."},
+            {'J', ".---"},
+            {'K', "-.-"},
+            {'L', ".-.."},
+            {'M', "--"},
+            {'N', "-."},
+            {'O', "---"},
+            {'P', ".--."},
+            {'Q', "--.-"},
+            {'R', ".-."},
+            {'S', "..."},
+            {'T', "-"},
+            {'U', "..-"},
+            {'V', "...-"},
+            {'W', ".--"},
+            {'X', "-..-"},
+            {'Y', "-.--"},
+            {'Z', "--.."},
+            {'0', "-----"},
+            {'1', ".----"},
+            {'2', "..---"},
+            {'3', "...--"},
+            {'4', "....-"},
+            {'5', "....."},
+            {'6', "-...."},
+            {'7', "--..."},
+            {'8', "---.."},
+            {'9', "----."}
+        };
+
+        static Dictionary<string, char> morseToCharDict = new Dictionary<string, char>();
+
+        static Translator()
+        {
+            foreach (var pair in morseCodeDict)
+                morseToCharDict[pair.Value] = pair.Key;
+        }
+
+        public string ToMorze(string message)
+        {
+            string newMes = "";
+            string tmpMes = message.ToUpper();
+            for (int i = 0; i < tmpMes.Length; i++)
+            {
+                if (morseCodeDict.ContainsKey(tmpMes[i]))
+                {
+                    newMes += morseCodeDict[tmpMes[i]] + " ";
+                }
+            }
+            return newMes.Trim();
+        }
+
+        public string ToNormal(string message)
+        {
+            string newMes = "";
+
+            string[] letters = message.Split(" ");
+
+            for(int i = 0; i < letters.Length; i++)
+                newMes += morseToCharDict[letters[i]];
+
+            return newMes;
+        }
+    }
+}
